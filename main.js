3c47ba4bf39665c1462846d0ccd3fdd8f1f370c5
matiasvlevi <matiasvlevi@gmail.com>
2023-02-06 22:01:19 -0500

    refactor

--- main.js
+++ main.js
@@ -1,104 +1,113 @@
-const fs = require('fs');
-
-let mnist = {};
-let dataArray = [];
-for (let i = 0; i < 7; i++) {
-  let batch = JSON.parse(fs.readFileSync(__dirname + '/dataset/mnist_batch_' + i + '.json', 'utf-8'));
-  dataArray = dataArray.concat(batch.data);
-}
-mnist.data = dataArray;
-
-
-
-
-
+const { readFileSync } = require('fs');
 
 class MNIST {
-  constructor() {
-    this.data = mnist.data;
-  }
+  constructor() { /* ... */ }
 }
 
-MNIST.makeData = function makeData(train, test, options) {
+MNIST.Load = function Load() {
+  let mnist = { data: [] };
+  let dataArray = [];
+
+  for (let i = 0; i < 7; i++) {
+    let batch = JSON.parse(
+        readFileSync(__dirname + '/dataset/mnist_batch_' + i + '.json', 'utf-8')
+    );
+    mnist.data = mnist.data.concat(batch.data);
+  }
+
+  return mnist;
+}
+
+MNIST.makeData = function makeData(train_length, test_length, options) {
+
+  const mnist = MNIST.Load();
+
   let shu = false;
   if (options !== undefined) {
     if (options.shuffle) {
       shu = options.shuffle;
     }
   }
-  let tr = [];
-  let te = [];
-  let len = train + test;
+  const dataset = { train: [], test: [] };
+
+  let len = train_length + test_length;
   if (len > 70000) {
     console.error('Please enter values that wont exceed 70 000');
   }
-  for (let i = 0; i < train; i++) {
-    tr.push(mnist.data[i]);
+
+  for (let i = 0; i < train_length; i++) {
+    dataset.train.push(mnist.data[i]);
   }
-  for (let i = train; i < len; i++) {
-    te.push(mnist.data[i]);
+
+  for (let i = train_length; i < len; i++) {
+    dataset.test.push(mnist.data[i]);
   }
+
   if (shu == true) {
-    tr = MNIST.shuffle(tr);
-    te = MNIST.shuffle(te);
+    dataset.train = MNIST.shuffle(dataset.train);
   }
-  return { traindata: tr, testdata: te };
+
+  return dataset;
 }
 
 MNIST.makeBatch = function makeBatch(batchCount, options) {
-  let shu = false;
+
+  const mnist = MNIST.Load();
+
+  let shuffle = false;
   let empty = false;
   if (options !== undefined) {
     if (options.shuffle !== undefined) {
-      shu = options.shuffle;
+      shuffle = options.shuffle;
     }
     if (options.empty !== undefined) {
       empty = options.empty;
     }
   }
-  let train_data = [];
-  let test_data = [];
+
+  const dataset = { train:[], test: [] };
 
   let trainCount = 60000;
   let testCount = 10000;
   let totalCount = trainCount + testCount;
 
   for (let i = 0; i < trainCount / batchCount; i++) {
-    let data = {
-      batch: []
-    }
+    let data = { batch: [] }
+
     for (let j = 0; j < batchCount; j++) {
-      let index = i * batchCount + j;
-      let image = mnist.data[index];
-      if (empty) {
-        image.label.push(0);
-      }
+      let image = mnist.data[i * batchCount + j];
+      
+      if (empty) image.label.push(0);
+      
       data.batch.push(image);
     }
-    train_data.push(data);
+
+    dataset.train.push(data);
 
   }
+
   for (let i = trainCount / batchCount; i < totalCount / batchCount; i++) {
-    let data = {
-      batch: []
-    }
+    let data = { batch: [] }
+
     for (let j = 0; j < batchCount; j++) {
-      let index = i * batchCount + j;
-      let image = mnist.data[index];
-      if (empty) {
-        image.label.push(0);
-      }
+      let image = mnist.data[i * batchCount + j];
+
+      if (empty) image.label.push(0);
+      
       data.batch.push(image);
     }
-    test_data.push(data);
+
+    dataset.test.push(data);
 
   }
-  if (shu == true) {
-    train_data = MNIST.shuffle(train_data);
-    te = MNIST.shuffle(test_data);
+
+  if (shuffle == true) {
+    dataset.train = MNIST.shuffle(dataset.train);
   }
-  return { traindata: train_data, testdata: test_data };
+
+  return dataset;
 }
+
 MNIST.shuffle = function shuffle(arr) {
   let currentId = arr.length;
 
@@ -111,6 +120,7 @@ MNIST.shuffle = function shuffle(arr) {
     arr[currentId] = arr[randId];
     arr[randId] = temp;
   }
+
   return arr;
 }
 
